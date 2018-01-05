1df9c0af0f41cc240b98b8af829e82c94daf4870
August-Z <402279076@qq.com>
2018-01-05 17:45:31 +0800

    拓展 scroll 组件，向外 $emit ScrollPosition

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -20,6 +20,10 @@
       data: {
         type: Array,
         default: null
+      },
+      listenScroll: {
+        type: Boolean,
+        default: false
       }
     },
     mounted () {
@@ -36,6 +40,13 @@
           probeType: this.probeType,
           click: this.click
         })
+
+        if (this.listenScroll) {
+          let me = this
+          this.scroll.on('scroll', (pos) => {
+            me.$emit('scroll', pos)
+          })
+        }
       },
       enable () {
         this.scroll && this.scroll.enable()
