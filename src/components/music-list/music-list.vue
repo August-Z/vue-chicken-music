938683083dd5d5e87982b337e4dcfeec082a0dd7
August-Z <402279076@qq.com>
2018-01-09 17:43:55 +0800

    完成 randomPlay action，实现随机播放歌曲以及 player 组件中的播放模式切换的 bug 修复

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -6,7 +6,7 @@
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="play-wrapper">
-        <div class="play" ref="playBtn" v-show="songs.length>0">
+        <div class="play" ref="playBtn" v-show="songs.length>0" @click="random">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
@@ -84,8 +84,14 @@
           index: index
         })
       },
+      random () {
+        this.randomPlay({
+          list: this.songs
+        })
+      },
       ...mapActions([
-        'selectPlay'
+        'selectPlay',
+        'randomPlay'
       ])
     },
     watch: {
