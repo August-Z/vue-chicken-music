5d9681bf4a26e5c65fc89e45736275b2d7c10cae
August-Z <402279076@qq.com>
2018-01-19 02:45:44 +0800

    排行页面

--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -4,7 +4,7 @@
       <ul>
         <li class="item">
           <div class="icon">
-            <img width="100" height="100" />
+            <img width="100" height="100"/>
           </div>
           <ul class="songlist">
             <li class="song">
@@ -20,8 +20,61 @@
 </template>
 
 <script type="text/ecmascript-6">
-  export default {}
+  import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
+  import {mapMutations} from 'vuex'
+
+  export default {
+    methods: {
+      ...mapMutations({})
+    },
+    components: {
+      Scroll,
+      Loading
+    }
+  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
+
+  .rank
+    position fixed
+    width 100%
+    top 88px
+    bottom 0
+    .toplist
+      height 100%
+      overflow hidden
+      .item
+        display flex
+        margin 0 20px
+        padding-top 20px
+        height 100px
+        &:last-child
+          padding-bottom 20px
+        .icon
+          flex 0 0 100px
+          width 100px
+          height 100px
+        .songlist
+          flex 1
+          display flex
+          flex-direction column
+          justify-content center
+          padding 0 20px
+          height 100px
+          overflow hidden
+          background $color-highlight-background
+          color $color-text-d
+          font-size $font-size-small
+          .song
+            no-wrap()
+            line-height 26px
+      .loading-container
+        position absolute
+        width 100%
+        top 50%
+        transform translateY(-50%)
 </style>
